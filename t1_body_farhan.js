ee462ab24e4dd130f100b1ffad33a82d568f6d64
recognitionissue <tasralasir@gmail.com>
2026-04-10 23:30:37 +0700

    Create t1_body_farhan.js

---
+++ t1_body_farhan.js
@@ -0,0 +1,18 @@
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
+
+//Paste Iklan Disini
+
+atOptions = {
+    'key' : '21e5b4a1da707b82541a4e38313c2bc0',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/21e5b4a1da707b82541a4e38313c2bc0/invoke.js"></scr' + 'ipt>');
+// Batas Iklan
+
+document.write('</div>');
+document.write('</div>');
