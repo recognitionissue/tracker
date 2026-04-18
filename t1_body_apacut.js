25f272a962c9157c06e9694bad3122ecd0d4fea2
recognitionissue <tasralasir@gmail.com>
2026-04-18 21:15:14 +0700

    Create t1_body_apacut.js

---
+++ t1_body_apacut.js
@@ -0,0 +1,18 @@
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
+
+//Paste Iklan Disini
+
+atOptions = {
+    'key' : '445fda48a9cc23713d88d6f918537b15',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/445fda48a9cc23713d88d6f918537b15/invoke.js"></scr' + 'ipt>');
+// Batas Iklan
+
+document.write('</div>');
+document.write('</div>');
