7c95f78b47ec31efa78c91c659c4d65ae7bec756
recognitionissue <tasralasir@gmail.com>
2026-05-17 22:36:04 +0700

    Update head_t1_apacut.js

--- head_t1_apacut.js
+++ head_t1_apacut.js
@@ -1,5 +1,5 @@
 ///// Arsae /////
-var arsT1Bing = 'https://apacut.teknodrem.com';
+var arsT1Bing = 'https://apacut2.teknodrem.com';
 
 // if(['google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
 	// window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + '&ip=' + window.ipAddressArsae
